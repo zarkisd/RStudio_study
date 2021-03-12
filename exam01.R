3dbba52fb545f0a6195df78d6f8298ad61e73447
zarkisd <xxjunhoxx@naver.com>
2021-03-12 23:13:09 +0900

    0312_연습

---
+++ exam01.R
@@ -0,0 +1,37 @@
+34
+3.14
+"25"
+TRUE
+Sys.Date()
+Sys.time()
+date()
+
+data <- "안녕하세요"
+data2 <- 30
+
+v1 <- c(1,2,3)
+v2 <- c("kim", "lee", "choi")
+v3 <- c(T,F,F)
+v4 <- seq(1,15)
+v5<-1:10
+v6<-seq(1,10,by=4)
+v7<-seq(1,10,length.out=4)
+
+x<- seq(1,3)
+x
+
+v8<-rep(x,times=3)
+v9<-rep(x,each=3)
+v8
+v9
+y<-seq(1,10)
+y
+y[3]
+y<-c("사과", "참외", "파인애플", "딸기", "포도")
+y[4]
+#딸기 포도 둘다 꺼내주세요
+y[4:5]
+y[c(4,5)]
+
+
+
---
+++ exam02_벡터연산 매트릭스.R
@@ -0,0 +1,10 @@
+x<-c(1,2,3)
+y<-c(3,4,5)
+a<-x+y
+b<-x-y
+c<-union(x,y) #합집합
+c
+d<-setdiff(x,y) # 차집합합
+d
+e<-intersect(x,y) # 교집합합
+e
